56344db6429e91cae788c3e0cb622d32dc1fa5fc
GrannyB52 <112523265+GrannyB52@users.noreply.github.com>
2022-12-05 16:59:38 -0500

    Add files via upload

---
+++ MapsandSets.js
@@ -0,0 +1,27 @@
+// {1,2,3,4}
+
+//"referee"
+
+//0: {Array(3) => true }
+//1: {Array(3) => false}
+
+//const hasDuplicate = arr => new Set(arr).size !== arr.length
+
+/* function isVowel(char) {
+  return "aeiou".includes(char);
+}
+
+function vowelCount(str) {
+  const VowelMap = new Map();
+  for (let char of str) {
+    let lowerCase = char.toLowerCase()
+    if (isVowel(lowerCase)) {
+      vowelMap.has(lowerCase) {
+        vowelMap.set(lowerCase, vowelMap.get(lowerCase) + 1);
+      } else {
+        vowelMap.set(lowerCase, 1);
+      }
+    }
+  }
+  return vowelMap;
+} */​
